1c793d99b283aea9cff5b01859d3293b985ca1b2
Matthew Buduschev <mysmplmail@gmail.com>
2019-11-05 22:57:52 +0300

    Paragraph doesn't implement Markup

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import markup.*;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        final Paragraph paragraph1 = new Paragraph(List.of(
+                new Strong(List.of(
+                        new Text("1"),
+                        new Strikeout(List.of(
+                                new Text("2"),
+                                new Emphasis(List.of(
+                                        new Text("3"),
+                                        new Text("4")
+                                )),
+                                new Text("5")
+                        )),
+                        new Text("6")
+                ))
+        ));
+    }
+
+}
